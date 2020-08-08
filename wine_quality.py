184fc8b216aae41229d90235b9c6360daa92dcf6
gangulyarin <gangulyarin@users.noreply.github.com>
2020-08-09 01:25:08 +0530

    plot accuracies

--- wine_quality.py
+++ wine_quality.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn import tree, metrics
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("winequality-red.csv", sep=";")
 quality_mapping = {
@@ -18,13 +19,30 @@ df = df.sample(frac=1).reset_index(drop=True)
 
 df_train = df[:1000]
 df_train = df_train.drop("quality", axis="columns")
-df_test = df[:599]
+df_test = df[599:]
 df_test = df_test.drop("quality", axis="columns")
 
-clf = tree.DecisionTreeClassifier(max_depth=3)
-clf.fit(df_train, df[:1000]["quality"])
+train_accuracies= []
+test_accuracies= []
 
-test_pred = clf.predict(df_test)
+for d in range(2,25):
+    #print("depth=",d)
+    clf = tree.DecisionTreeClassifier(max_depth=d)
+    clf.fit(df_train, df[:1000]["quality"])
 
-train_accuracy = metrics.accuracy_score(df[:599]["quality"], test_pred)
-print(train_accuracy)
+    train_pred = clf.predict(df_train)
+    test_pred = clf.predict(df_test)
+
+    train_accuracy = metrics.accuracy_score(df[:1000]["quality"], train_pred)
+    test_accuracy = metrics.accuracy_score(df[599:]["quality"], test_pred)
+
+    train_accuracies.append(train_accuracy)
+    test_accuracies.append(test_accuracy)
+
+plt.figure(figsize=(10,10))
+plt.plot(train_accuracies, label="train accuracy")
+plt.plot(test_accuracies, label="test accuracy")
+#plt.xticks(range(0, 26, 5))
+plt.xlabel("max depth")
+plt.ylabel("accuracy")
+plt.show()
